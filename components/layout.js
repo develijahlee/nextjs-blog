adc3f883bcdd995a05538e92f67bacc05ae99ac3
Elijah Lee <develijahlee@gmail.com>
2020-11-26 11:33:38 +0900

    Unhandled Runtime Error CSS

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,14 @@
 import styles from './layout.module.css';
 
 export default function Layout({ children }) {
-  return <div className={styles.container}>{children}</div>;
+  return (
+    <div className={`container ${styles.container}`}>
+      {children}
+      <style jsx>{`
+        .container {
+          background: yellow;
+        }
+      `}</style>
+    </div>
+  );
 }
