bf9d1258128b24f2bed87c8fa4a6f5d13cf72ebb
Elijah Lee <develijahlee@gmail.com>
2020-11-27 18:59:26 +0900

    generate dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,6 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.scss';
 import { getSortedPostsData } from '../lib/posts';
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
@@ -41,4 +32,14 @@ export default function Home({ allPostsData }) {
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  console.log(allPostsData);
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
